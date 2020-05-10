a7059e91fc84b9940d5a4b358d2f8e9679ce8113
DanielHalvarsson <halvarsson.daniel@gmail.com>
2020-05-10 23:14:57 +0200

    Julia code

--- JuliaSparse.jl
+++ JuliaSparse.jl
@@ -1,12 +1,9 @@
-# using SparseArrays
-
+using SparseArrays
 a = [0.5 1.0 0.0 ; 2.0 0.0 0.0 ; 0.0 0.0 0.0]
 b = sparse(a)
 c = [1 2 3]
-b*c
+Array(c*b)
 
 using Plots
 gr(fmt=png)
-plot(sqrt, 0, 10, label="sq.rot(x)")
-
-    print("ϵ=10")
+plot(sin, 0, 10, label="sin(x)")
