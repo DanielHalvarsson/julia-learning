9750eab6ba5d30b8efdd9b582ebf954d46cfa1e7
DanielHalvarsson <halvarsson.daniel@gmail.com>
2021-02-07 22:51:34 +0100

    JuliaSB

--- JuliaSparse.jl
+++ JuliaSparse.jl
@@ -7,7 +7,15 @@ b = sparse(a)
 c = [1 2 3]
 Array(c*b)
 
+# Just a plot
 using Plots
 gr(fmt=png)
 plot(sin, 0, 10, label="sin(x)")
 print("This is a plot")
+
+
+# Data base
+using JuliaDB
+
+t = table(rand(Bool,10), rand(10),pkey = 1)
+
